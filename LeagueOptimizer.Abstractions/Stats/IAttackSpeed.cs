1577246d76e2635426068768481e54d13d9167ed
lucad <luca@trenz.de>
2024-11-05 19:08:14 +0100

    removed debug code

--- LeagueOptimizer.Abstractions/Stats/IAttackSpeed.cs
+++ LeagueOptimizer.Abstractions/Stats/IAttackSpeed.cs
@@ -22,12 +22,6 @@ public class AttackSpeed : IPerLevelStat
         var g = Growth;
         var n = level.Value;
 
-        Console.Out.WriteLine("Base: " + Base);
-        Console.Out.WriteLine("Bonus: " + Bonus);
-        Console.Out.WriteLine("Growth: " + Growth);
-        Console.Out.WriteLine("Ratio: " + Ratio);
-        // formula as string with values:
-        Console.Out.WriteLine($"{Base} + ({Bonus} + {g} * ({n} - 1) * ({IPerLevelStat.SchizoPerLevelMultiplier1} + {IPerLevelStat.SchizoPerLevelMultiplier2} * ({n} - 1))) * {Ratio}");
         return Base + (Bonus + g * (n - 1) * (IPerLevelStat.SchizoPerLevelMultiplier1 + IPerLevelStat.SchizoPerLevelMultiplier2 * (n - 1))) * Ratio;
     }
 }
