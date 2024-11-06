b1516d89a42b238239373d61a6d9c1909e7a5f9f
Luca Diegel <Luca@trenz.de>
2024-11-07 00:11:32 +0100

    removed wrong implementation

--- LeagueOptimizer.Abstractions/Stats/Stat.cs
+++ LeagueOptimizer.Abstractions/Stats/Stat.cs
@@ -6,7 +6,6 @@ public class Stat(decimal baseValue) : IStat
 {
     public decimal Base { get; set; } = baseValue;
     public decimal Bonus { get; set; } = 0;
-    public decimal Total { get; set; } = baseValue;
 
     public Stat(StatData data) : this(data.Base){}
 }
