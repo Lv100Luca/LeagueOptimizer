f28937e677f6fdea89c0086e65896d7965494170
Luca Diegel <Luca@trenz.de>
2024-11-07 00:15:40 +0100

    added normal attack calculation

--- LeagueOptimizer.CLI/Program.cs
+++ LeagueOptimizer.CLI/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using LeagueOptimizer.Abstractions.Champions;
+using LeagueOptimizer.Models.Champions.Caitlyn;
 using LeagueOptimizer.Services;
 using Microsoft.Extensions.Logging;
 
@@ -12,11 +13,21 @@ public static class Program
     {
         var reader = new StatReader(new Logger<StatReader>(new LoggerFactory()));
         var factory = new ChampionFactory(reader, new Logger<ChampionFactory>(new LoggerFactory()));
-        var cait = factory.Build(ChampionNames.Caitlyn);
+        // todo fix casting
+        var cait = (Caitlyn)factory.Build(ChampionNames.Caitlyn);
 
         cait.Level = Level.From(3);
+
+        cait.AttackDamage.Base = 100;
+        cait.AttackDamage.Growth = 0;
         cait.AttackSpeed.Bonus = 0.25m;
+        cait.AbilityPower.Bonus = 100m;
+        cait.CritChance.Bonus = 0.50m;
 
         Console.WriteLine(cait);
+
+        Console.WriteLine("=================");
+        Console.WriteLine("Normal Attack Damage:");
+        Console.WriteLine(cait.CalculateNormalAttackDamage());
     }
 }
--- LeagueOptimizer.Models/Champions/Caitlyn/Caitlyn.cs
+++ LeagueOptimizer.Models/Champions/Caitlyn/Caitlyn.cs
@@ -1,4 +1,5 @@
-﻿using LeagueOptimizer.Abstractions.Champions.Data;
+﻿using LeagueOptimizer.Abstractions;
+using LeagueOptimizer.Abstractions.Champions.Data;
 using Microsoft.Extensions.Logging;
 
 namespace LeagueOptimizer.Models.Champions.Caitlyn;
@@ -8,4 +9,15 @@ public class Caitlyn(ChampionData data, ILogger<Caitlyn> logger) : Champion(data
     public const string FilePath = "Champions/Caitlyn/Caitlyn.json";
 
     override public string Name { get; set; } = "Caitlyn";
+
+    // todo move to Champion.cs
+    public CriticalDamageResult CalculateNormalAttackDamage()
+    {
+        // consider non crit, crit and average damage
+        const decimal normalAttackScaling = 1.0m;
+
+        var dmg = AttackDamage.Total(Level) * normalAttackScaling;
+
+        return new CriticalDamageResult(DamageType.Physical, dmg, this);
+    }
 }
