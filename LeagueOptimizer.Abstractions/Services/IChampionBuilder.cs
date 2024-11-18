c7dade197e4d72f920b094ea4b89a1573a81e14c
Luca Diegel <Luca@trenz.de>
2024-11-18 13:14:41 +0100

    deleted unused classes

--- LeagueOptimizer.Abstractions/Services/IChampionBuilder.cs
+++
@@ -1,6 +0,0 @@
-using LeagueOptimizer.Abstractions.Champions;
-namespace LeagueOptimizer.Abstractions.Services;
-
-public interface IChampionFactory {
-    Champion Build(ChampionNames championName);
-}
