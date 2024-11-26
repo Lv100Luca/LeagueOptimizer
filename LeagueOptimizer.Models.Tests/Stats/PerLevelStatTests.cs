006d4569f6f7bd336a8c4e9878a624c327c6f771
Luca Diegel <Luca@trenz.de>
2024-11-26 11:21:22 +0100

    added tests for per level stat calculations

---
+++ LeagueOptimizer.Models.Tests/Stats/PerLevelStatTests.cs
@@ -0,0 +1,112 @@
+using LeagueOptimizer.Abstractions.Champions;
+using LeagueOptimizer.Abstractions.Champions.Data;
+using LeagueOptimizer.Models.Champions.Stats;
+
+namespace LeagueOptimizer.Models.Tests.Stats;
+
+public class PerLevelStatTests
+{
+    // this test uses example stats from caitlyn
+    [TestCase(1, 580)]
+    [TestCase(2, 657.04)]
+    [TestCase(3, 737.83)]
+    [TestCase(4, 822.36)]
+    [TestCase(5, 910.63)]
+    [TestCase(6, 1002.65)]
+    [TestCase(7, 1098.42)]
+    [TestCase(8, 1197.93)]
+    [TestCase(9, 1301.18)]
+    [TestCase(10, 1408.18)]
+    [TestCase(11, 1518.93)]
+    [TestCase(12, 1633.42)]
+    [TestCase(13, 1751.65)]
+    [TestCase(14, 1873.63)]
+    [TestCase(15, 1999.36)]
+    [TestCase(16, 2128.82)]
+    [TestCase(17, 2262.04)]
+    [TestCase(18, 2399)]
+    public void TestPerLevelStatCalculation(int level, decimal expectedValue)
+    {
+        const decimal baseValue = 580;
+        const decimal growth = 107;
+
+        var statData = new StatData
+        {
+            Base = baseValue,
+            Growth = growth
+        };
+
+        var perLevelStat = new PerLevelStat(statData) { Level = Level.From(level) };
+
+        Assert.That(perLevelStat.Total, Is.EqualTo(expectedValue).Within(0.1m));
+    }
+
+    // this test uses the example stats from Alistar
+    [Test]
+    public void TestPerLevelStatCalculationAlistar()
+    {
+        const decimal baseValue = 685;
+        const decimal growth = 120;
+        var level = Level.From(2);
+
+        var statData = new StatData
+        {
+            Base = baseValue,
+            Growth = growth
+        };
+
+        const decimal expectedValue = 771.4m;
+
+        var perLevelStat = new PerLevelStat(statData, level);
+
+        Assert.That(perLevelStat.Total, Is.EqualTo(expectedValue).Within(0.1m));
+    }
+
+
+    // this test uses the example given on the wiki using Volibear
+    [Test]
+    public void TestAttackSpeedCalculationVolibear()
+    {
+        const decimal baseValue = 0.625m;
+        const decimal growth = 0.02m;
+        const decimal ratio = 0.7m;
+        const decimal bonusAttackSpeed = 0.25m;
+
+        var level = Level.From(3);
+
+        var statData = new AttackSpeedData
+        {
+            Base = baseValue,
+            Growth = growth,
+            Ratio = ratio,
+        };
+
+        var attackSpeed = new AttackSpeed(statData, level) { Bonus = bonusAttackSpeed };
+
+        const decimal expectedValue = 0.82065m;
+
+        Assert.That(attackSpeed.Total, Is.EqualTo(expectedValue).Within(0.001m));
+    }
+
+    [Test]
+    public void TestStatCalculationWithBonus()
+    {
+        const decimal baseValue = 580;
+        const decimal growth = 107;
+        const decimal bonus = 475;
+
+        var level = Level.From(8);
+
+        var statData = new StatData
+        {
+            Base = baseValue,
+            Growth = growth
+        };
+
+        var perLevelStat = new PerLevelStat(statData, level) { Bonus = bonus };
+
+        const decimal expectedValue = 1518.93m;
+
+        Assert.That(perLevelStat.Total, Is.EqualTo(expectedValue).Within(0.1m));
+    }
+}
