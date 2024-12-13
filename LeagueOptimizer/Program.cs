1727d0835ed1c58a7183a7b9c15b07eb04cb14c6
Luca Diegel <Luca@trenz.de>
2024-12-13 16:13:17 +0100

    log hosting env

--- LeagueOptimizer/Program.cs
+++ LeagueOptimizer/Program.cs
@@ -8,4 +8,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+// get hosting environment
+var hostEnv = builder.HostEnvironment;
+
+Console.Out.WriteLine("Host env: "+ hostEnv.BaseAddress);
+
+await app.RunAsync();
