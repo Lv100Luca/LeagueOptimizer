1132bd3edae8e7a5bee9e41430b6fb01d9c97d45
Luca Diegel <Luca@trenz.de>
2024-12-13 11:35:45 +0100

    added required hints

--- LeagueOptimizer.Models/Champions/Caitlyn/AbilityData/CaitlynAbilityData.cs
+++ LeagueOptimizer.Models/Champions/Caitlyn/AbilityData/CaitlynAbilityData.cs
@@ -2,56 +2,56 @@ namespace LeagueOptimizer.Models.Champions.Caitlyn.AbilityData;
 
 public class CaitlynAbilityData
 {
-    public PassiveData Passive { get; set; }
-    public SpellQData SpellQ { get; set; }
-    public SpellWData SpellW { get; set; }
-    public SpellEData SpellE { get; set; }
-    public SpellRData SpellR { get; set; }
+    public required PassiveData Passive { get; set; }
+    public required SpellQData SpellQ { get; set; }
+    public required SpellWData SpellW { get; set; }
+    public required SpellEData SpellE { get; set; }
+    public required SpellRData SpellR { get; set; }
 
     public class PassiveData
     {
-        public List<decimal> ChampionTotalAdScaling { get; set; }
-        public List<decimal> TotalAdScaling { get; set; }
-        public decimal CritScaling { get; set; }
-        public decimal IeCritBonusScaling { get; set; }
+        public required List<decimal> ChampionTotalAdScaling { get; set; }
+        public required List<decimal> TotalAdScaling { get; set; }
+        public required decimal CritScaling { get; set; }
+        public required decimal IeCritBonusScaling { get; set; }
     }
 
     public class SpellQData
     {
-        public List<int> Cost { get; set; }
-        public List<int> Cooldown { get; set; }
-        public List<int> BaseDmg { get; set; }
-        public List<decimal> TotalAdScaling { get; set; }
-        public decimal ReducedDamageMultiplier { get; set; }
+        public required List<int> Cost { get; set; }
+        public required List<int> Cooldown { get; set; }
+        public required List<int> BaseDmg { get; set; }
+        public required List<decimal> TotalAdScaling { get; set; }
+        public required decimal ReducedDamageMultiplier { get; set; }
     }
 
     public class SpellWData
     {
-        public int Cost { get; set; }
-        public decimal Cooldown { get; set; }
-        public List<int> Recharge { get; set; }
-        public List<int> Duration { get; set; }
-        public List<int> MaximumCharges { get; set; }
-        public List<int> BaseDmg { get; set; }
-        public decimal BonusAdScaling { get; set; }
+        public required int Cost { get; set; }
+        public required decimal Cooldown { get; set; }
+        public required List<int> Recharge { get; set; }
+        public required List<int> Duration { get; set; }
+        public required List<int> MaximumCharges { get; set; }
+        public required List<int> BaseDmg { get; set; }
+        public required decimal BonusAdScaling { get; set; }
     }
 
     public class SpellEData
     {
-        public int Cost { get; set; }
-        public List<int> Cooldown { get; set; }
-        public List<int> BaseDmg { get; set; }
-        public decimal ApScaling { get; set; }
-        public decimal SlowAmount { get; set; }
-        public decimal SlowDuration { get; set; }
+        public required int Cost { get; set; }
+        public required List<int> Cooldown { get; set; }
+        public required List<int> BaseDmg { get; set; }
+        public required decimal ApScaling { get; set; }
+        public required decimal SlowAmount { get; set; }
+        public required decimal SlowDuration { get; set; }
     }
 
     public class SpellRData
     {
-        public int Cost { get; set; }
-        public int Cooldown { get; set; }
-        public List<int> BaseDmg { get; set; }
-        public decimal BonusAdScaling { get; set; }
-        public decimal CritScaling { get; set; }
+        public required int Cost { get; set; }
+        public required int Cooldown { get; set; }
+        public required List<int> BaseDmg { get; set; }
+        public required decimal BonusAdScaling { get; set; }
+        public required decimal CritScaling { get; set; }
     }
 }
