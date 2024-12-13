84b802623f0d42eabcd4e2de21c7fc6f2ced94c8
Luca Diegel <Luca@trenz.de>
2024-12-13 11:24:51 +0100

    push

--- LeagueOptimizer.CLI/Program.cs
+++ LeagueOptimizer.CLI/Program.cs
@@ -42,7 +42,7 @@ public static class Program
 
         Console.WriteLine(cait.CalculateResistanceDamageReduction(target, DamageType.Physical));
     }
-    //
+
 
     private static void TestResistCalculation1()
     {
