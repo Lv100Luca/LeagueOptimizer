1035e447f642c2aa1de57f3eb352099bd6da58d5
Luca Diegel <Luca@trenz.de>
2024-12-16 17:41:50 +0100

    added constructors for target dummy

--- LeagueOptimizer.Models/TargetDummy.cs
+++ LeagueOptimizer.Models/TargetDummy.cs
@@ -1,11 +1,33 @@
 using LeagueOptimizer.Abstractions.Champions;
 using LeagueOptimizer.Abstractions.Champions.Stats;
+using LeagueOptimizer.Models.Champions.Stats;
 
 namespace LeagueOptimizer.Models;
 
 public class TargetDummy : ITarget
 {
-    public required IResource Health { get; set; }
-    public required IResistance Armor { get; set; }
-    public required IResistance MagicResist { get; set; }
+    public IResource Health { get; set; }
+    public IResistance Armor { get; set; }
+    public IResistance MagicResist { get; set; }
+
+    public TargetDummy(decimal health, decimal armor, decimal magicResist)
+    {
+        Health = new Resource(health);
+        Health.Current = health;
+        Armor = new Resistance(armor);
+        MagicResist = new Resistance(magicResist);
+    }
+
+    public TargetDummy(decimal health, decimal armor, decimal magicResist, decimal currentHealthPercent = 100)
+    {
+        Health = new Resource(health);
+        Health.Current = health * currentHealthPercent / 100;
+        Armor = new Resistance(armor);
+        MagicResist = new Resistance(magicResist);
+    }
+
+    public TargetDummy()
+    {
+
+    }
 }
